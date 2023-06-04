b92d339c3016b687b59b2dda21633ce243b6d263
Mateo1O <compla1@hotmail.com>
2023-06-04 08:38:03 -0700

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for a in range(0, 10):
+    for b in range(a, 10):
+        if a == 8 and b == 9:
+            print("{}{}".format(a, b))
+        elif a != b:
+            print("{}{}".format(a, b), end=", ")
