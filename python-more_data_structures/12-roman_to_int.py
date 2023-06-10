4c27b0af2270cb39929a06568f5e3ec5134f3e90
Mateo1O <compla1@hotmail.com>
2023-06-10 12:18:31 -0700

    name error

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,10 +4,10 @@ def roman_to_int(roman_string):
     if not roman_string or type(roman_string) is not str:
         return 0
     iT = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
-    aList = [iT.get(x) for x in roman_string]
-    for idx, char in enumerate(aList):
-        if (idx < len(aList) - 1):
-            if aList[idx + 1] > char:
+    newlist = [iT.get(x) for x in roman_string]
+    for idx, char in enumerate(newlist):
+        if (idx < len(newlist) - 1):
+            if newlist[idx + 1] > char:
                 sum -= char
             else:
                 sum += char
