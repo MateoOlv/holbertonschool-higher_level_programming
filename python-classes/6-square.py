042dbf58a88271b95175918b353b6ea13a58d986
MateoOlv <6304@holbertonstudents.com>
2023-06-12 09:38:24 -0700

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -35,7 +35,7 @@ class Square:
     def position(self, value):
         if (type(value) != tuple or len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if (type(value[0]) != int or type(value[1]) != int):
+        if (type(value[0]) < 0 or type(value[1]) < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = (value[0], value[1])
     """Define myprint"""
