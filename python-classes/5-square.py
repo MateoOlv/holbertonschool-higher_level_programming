56e13b65afe815c0d432e5bb4f3dbd6f57aeaed3
MateoOlv <6304@holbertonstudents.com>
2023-06-12 08:22:14 -0700

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,2 +1,36 @@
 #!/usr/bin/python3
-size
+""" Define class """
+
+
+class Square:
+    """Define init."""
+
+    def __init__(self, size=0):
+        self.__size = size
+    """Define size property"""
+    @property
+    def size(self):
+        return (self.__size)
+    """Define size"""
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise (TypeError("size must be an integer"))
+        if value < 0:
+            raise (ValueError("size must be >= 0"))
+        self.__size = value
+
+    """Define area"""
+
+    def area(self):
+        return (self.__size**2)
+
+    """Define myprint"""
+
+    def my_print(self):
+        for i in range(self.size):
+            for i in range(self.size):
+                print("#", end="")
+            print()
+        if self.size == 0:
+            print("")
