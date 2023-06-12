8c4ecd00fbcbf7f529885ab74bcf3a72056de6fb
MateoOlv <6304@holbertonstudents.com>
2023-06-12 08:12:21 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
-size
+""" Define class """
+
+
+class Square:
+    """Define init."""
+
+    def __init__(self, size=0):
+        self.__size = size
+    """Define size property"""
+    @property
+    def size(self):
+        return (self.__size)
+    """Define size"""
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise (TypeError("size must be an integer"))
+        if value < 0:
+            raise (ValueError("size must be >= 0"))
+        self.__size = value
+
+    """Define area"""
+
+    def area(self):
+        return (self.__size**2)
