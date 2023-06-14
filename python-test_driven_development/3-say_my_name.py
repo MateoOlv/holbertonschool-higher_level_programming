9e7ac69bf61ceb0f72d7599741ba5699ab52f50a
MateoOlv <6304@holbertonstudents.com>
2023-06-14 11:18:12 -0700

    3-

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
+"""
+Define Error handler
+"""
+
+
 def Error(first_name, last_name):
+    """
+    Error cases
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
         raise TypeError("last_name must be a string")
 
 
+"""Define say my name"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Call Error handler
+    And
+    Print the name with the last name
+    """
     Error(first_name, last_name)
     print("My name is {} {}". format(first_name, last_name))
