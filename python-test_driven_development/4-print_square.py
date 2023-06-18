0de9fe16548b9f3791e73dab007be09e73db2133
Mateo1O <compla1@hotmail.com>
2023-06-18 09:38:42 -0700

    update

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+'''
+4-print_square module
+'''
+
+
 def print_square(size):
+    '''
+    prints a square of size
+    '''
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for a in range(size):
+        print("{}".format("#" * size))
