22bbfa2c3bb7fcafaa7aad06d59e3be7a26da7c1
Mateo1O <compla1@hotmail.com>
2023-06-18 09:54:00 -0700

    test files fix

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""
+Text indent mod
+"""
 def text_indentation(text):
+    """
+    check error
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    """check text indentation"""
+    pon = 0
+    for idx, i in enumerate(text):
+        if i in '?:.':
+            print(text[pon:idx + 1].strip() + '\n')
+            pon = idx + 1
+    if not pon:
+        print(text, end='')
+    elif pon is not len(text):
+        print(text[pon:idx + 1].strip(), end='')
