9f3a1aa5e2551a2001c635d4d6d0ef0ae9bbcbf4
MateoOlv <6304@holbertonstudents.com>
2023-06-21 10:06:33 -0700

    6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""def load from json file"""
+import json
+
+
 def load_from_json_file(filename):
+    """reads and write loads of f"""
+    with open(filename, mode='r', encoding='utf-8') as f:
+        f.write(json.loads(f))
