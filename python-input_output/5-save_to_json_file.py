e370d78b13e590c1b02f50fe5605756c55212eca
MateoOlv <6304@holbertonstudents.com>
2023-06-21 10:03:38 -0700

    5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""Define save to json file"""
+import json
+
+
 def save_to_json_file(my_obj, filename):
+    """Opnes the file in writing mode and write"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        f.write(json.dumps(my_obj))
