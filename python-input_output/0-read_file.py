950dab1a1352969aed9543d0b964544ae1428f00
MateoOlv <6304@holbertonstudents.com>
2023-06-21 06:30:16 -0700

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Define read file"""
+"""
+Define read file
+"""
 
 
 def read_file(filename=""):
-    """Open file and read"""
+    """
+    Open file and read
+    """
     with open(filename, mode='r', encoding='utf-8') as f:
         for line in f:
             print(line, end="")
