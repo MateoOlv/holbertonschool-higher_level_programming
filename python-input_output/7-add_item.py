c44d99fd16b3f091c182ba3d7161472f92460d75
Mateo1O <compla1@hotmail.com>
2023-06-25 01:02:38 -0700

    a

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-save_to_json_file
+"""imports"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    jsonList = load_from_json_file("add_item.json")
+except:
+    jsonList = []
+
+for i in sys.argv[1:]:
+    jsonList.append(i)
+save_to_json_file(jsonList, "add_item.json")
