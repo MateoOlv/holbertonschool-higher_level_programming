62129e13a7122774152cfd313ce59fb34ae5b663
Mateo1O <compla1@hotmail.com>
2023-07-01 15:15:14 -0700

    almost a circle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,9 +7,10 @@ from os import path
 class Base:
     """Base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id == None:
-            Base.__nb_objects +=1
+        if id is None:
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
             self.id = id
