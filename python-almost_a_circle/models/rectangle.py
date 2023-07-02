90d1e79e73d07fd683dc998ac2816b25e113a252
Mateo1O <compla1@hotmail.com>
2023-07-02 11:27:52 -0700

    chmod?

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,43 +1,95 @@
 #!/usr/bin/python3
+"""imports"""
 from models.base import Base
 
 
 class Rectangle(Base):
+    """Rect base"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
+        """init values"""
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
+
+    def to_dictionary(self):
+        """returns dict representation"""
+        dic = {'id': self.id, 'width': self.width, 'height': self.height,
+               'x': self.x, 'y': self.y}
+        return dic
+
+    def area(self):
+        return self.width * self.height
+
+    def update(self, *args, **kw):
+        """update the values"""
+        if args:
+            i = 0
+            keys = ['id', 'width', 'height', 'x', 'y']
+            for arg in args:
+                setattr(self, keys[i], arg)
+                i += 1
+        elif kw:
+            for key, value in kw.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
+    def display(self):
+        """prints a rect"""
+        print('\n' * self.y, end="")
+        print(''.join(' ' * self.x + '#' * self.width + '\n'
+                      for times in range(self.height)), end="")
+
+    def __str__(self):
+        """gets rect"""
+        return (f"[Rectangle] ({self.id})"
+                f" {self.x}/{self.y} - {self.width}/{self.height}")
 
     @property
     def width(self):
         return self.__width
 
-    @width.setter
-    def width(self, value):
-        self.__width = value
-
     @property
     def height(self):
         return self.__height
-
-    @height.setter
-    def height(self, value):
-        self.__height = value
 
     @property
     def x(self):
         return self.__x
 
-    @x.setter
-    def x(self, value):
-        self.__x = value
-
     @property
     def y(self):
         return self.__y
 
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 1:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 1:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
     @y.setter
     def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
